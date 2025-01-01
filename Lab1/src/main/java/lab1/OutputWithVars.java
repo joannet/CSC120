3163e4aad6586d61530a306b4d1a34d0c2963527
Prof. Thacker <joanne.thacker@ncc.edu>
2025-01-01 14:30:11 -0500

    Create OutputWithVars.java

---
+++ Lab1/src/main/java/lab1/OutputWithVars.java
@@ -0,0 +1,18 @@
+package lab1;
+import java.util.Scanner;
+
+public class OutputWithVars {
+
+	public static void main(String[] args)
+	{
+		Scanner scnr = new Scanner(System.in);
+		int userNum;
+		
+		System.out.println("Enter an integer:");
+		userNum = scnr.nextInt();
+		
+		/* Type your code here */
+				
+		scnr.close();
+	}
+}
