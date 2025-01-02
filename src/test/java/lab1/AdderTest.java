83bcc55daa31d004c514e43a26545a1a51d84320
Prof. Thacker <joanne.thacker@ncc.edu>
2025-01-02 10:20:45 -0500

    Update and rename Lab1/src/test/java/lab1/CaffeineTest.java to src/test/java/lab1/AdderTest.java

--- src/test/java/lab1/AdderTest.java
+++ src/test/java/lab1/AdderTest.java
@@ -3,10 +3,10 @@ package lab1;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class CaffeineTest {
+public class AdderTest {
     @Test
     public void testAdd() {
-        Caffeine adder = new Caffeine();
+        Adder adder = new Adder();
         assertEquals(5, adder.add(2, 3));
         assertEquals(0, adder.add(-1, 1));
         assertEquals(-5, adder.add(-2, -3));
