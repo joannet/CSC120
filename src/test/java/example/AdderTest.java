0ac2a626a58234c8a666d23c332ad8faa1034899
Prof. Thacker <joanne.thacker@ncc.edu>
2025-01-01 19:01:25 -0500

    Create AdderTest.java

---
+++ src/test/java/example/AdderTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AdderTest {
+    @Test
+    public void testAdd() {
+        Adder adder = new Adder();
+        assertEquals(5, adder.add(2, 3));
+        assertEquals(0, adder.add(-1, 1));
+        assertEquals(-5, adder.add(-2, -3));
+    }
+}
