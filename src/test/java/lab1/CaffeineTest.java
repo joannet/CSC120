9159b9b99f6e037a810b16174b6eb77c0e04195c
Prof. Thacker <joanne.thacker@ncc.edu>
2025-01-01 19:46:21 -0500

    Create CaffeineTest.java

---
+++ src/test/java/lab1/CaffeineTest.java
@@ -0,0 +1,14 @@
+package lab1;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CaffeineTest {
+    @Test
+    public void testAdd() {
+        Caffeine adder = new Caffeine();
+        assertEquals(5, adder.add(2, 3));
+        assertEquals(0, adder.add(-1, 1));
+        assertEquals(-5, adder.add(-2, -3));
+    }
+}
