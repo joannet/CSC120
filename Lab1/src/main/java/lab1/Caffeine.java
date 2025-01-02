851417ec789ee92ffa766f44125ccd9b85fdaec0
Prof. Thacker <joanne.thacker@ncc.edu>
2025-01-01 20:56:53 -0500

    Update Caffeine.java

--- Lab1/src/main/java/lab1/Caffeine.java
+++ Lab1/src/main/java/lab1/Caffeine.java
@@ -1,33 +1,8 @@
 package lab1;
 
-import java.util.Scanner; 
-
 public class Caffeine {
-	
-
-	public static void main(String[] args) {
-	   
-      
-		Scanner scnr = new Scanner(System.in);      
-		double caffeineMg; // "double" supports floating-point like 75.5, versus int for integers like 75.       
-		double halfAmt;
-      
-      
-		System.out.println("Enter the amount of caffeine in mg.");      
-		caffeineMg = scnr.nextDouble(); 
-      
-		halfAmt = caffeineMg / 2;
-		System.out.println("After 6 hours: " + halfAmt + " mg");
-      
-		halfAmt = halfAmt / 2;
-		System.out.println("After 12 hours: " + halfAmt + " mg");
-      
-		halfAmt = halfAmt / 2;
-		System.out.println("After 18 hours: " + halfAmt + " mg");
-            
-		/* Type your code here. */
-      
-		scnr.close();
-   
-	}
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
+
