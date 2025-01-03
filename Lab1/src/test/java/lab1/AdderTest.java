8c30f8502899c4a9e5e41faf6b611a7335996d8c
Prof. Thacker <joanne.thacker@ncc.edu>
2025-01-03 12:55:18 -0500

    Update AdderTest.java

--- Lab1/src/test/java/lab1/AdderTest.java
+++ Lab1/src/test/java/lab1/AdderTest.java
@@ -10,11 +10,14 @@ public class AdderTest {
 
         // Test case: 2 + 3 = 5
         assertEquals("Expected 2 + 3 to equal 5", 5, adder.add(2, 3));
+        System.out.println("Test case 2 + 3 = 5 passed.");
 
         // Test case: -1 + 1 = 0
         assertEquals("Expected -1 + 1 to equal 0", 0, adder.add(-1, 1));
+        System.out.println("Test case -1 + 1 = 0 passed.");
 
         // Test case: -2 + -3 = -5
         assertEquals("Expected -2 + -3 to equal -5", -5, adder.add(-2, -3));
+        System.out.println("Test case -2 + -3 = -5 passed.");
     }
 }
